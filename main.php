ebc1b832cb7123403d99ad0d023273de8c5f57cd
Seiyaaaaaaaaaaaaaaa ( Brian O. Blaza) <brianblaza2003@gmail.com>
2024-08-30 13:17:02 +0800

    Add files via upload

---
+++ main.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Group 4 Team Portfolio</title>
+    <link rel="stylesheet" href="index.css">
+    <script src="index.js"></script>
+</head>
+<body>
+    <h1>Group 4 Team Profile</h1>
+    <button onclick="showContent('content1')">Brian O. Blaza</button>
+    <button onclick="showContent('content2')">Mark Allen G. Bayona</button>
+    <button onclick="showContent('content3')">Jhovella Mae G. Buendia</button>
+    <button onclick="showContent('content4')">Katrina M. Macalinao</button>
+    <button onclick="showContent('content5')">Jonalyn P. Madrigal</button>
+
+    <div id="content1" class="content">
+        <h2>Brian O. Blaza</h2>
+        <h3>My Social Media Account:</h3>
+        <p>Hi, my name is Brian O. Blaza from Team 4. I am 21 years old, and we are currently working on this webpage. As the team leader, we constructed this webpage with manner and professionalism. I have experience as an IT Specialist in a private company and in back-end programming, and I believe we will inspire others.</p>
+        <img src="Brian.jpg" alt="Brian O. Blaza">
+        <button id="fb" onclick="window.location.href='https://www.facebook.com/haruhikoooooo?mibextid=ZbWKwL'"></button>
+        <button id="insta" onclick="window.location.href='https://www.instagram.com/seiyaaaaaaaaaaaaaaaaaaaaaaaaaa?igsh=dGp1aTdiMTRzMg=='"></button>
+        <button id="github" onclick="window.location.href='https://github.com/Seiyaaaaaaaaaaaaaaa'"></button>
+    </div>
+    
+    <div id="content2" class="content">
+        <h2>Mark Allen G. Bayona</h2>
+        <h3>My Social Media Account:</h3>
+        <p>My name is Mark Allen Bayona. I am 22, from Katarungan Village 2. I am part of Team 4 of BSIT 3L.</p>
+        <img src="alien.jpg" alt="Mark Allen G. Bayona">
+        <button id="fb" onclick="window.location.href='https://www.facebook.com/mark.bayona.3?mibextid=ZbWKwL'"></button>
+        <button id="insta" onclick="window.location.href='https://www.instagram.com/llenofficial_?igsh=MXdvZjUxYzdiM3hmOQ=='"></button>
+        <button id="github" onclick="window.location.href='https://github.com/len-mark27'"></button>
+    </div>
+    
+    <div id="content3" class="content">
+        <h2>Jhovella Mae G. Buendia</h2>
+        <h3>My Social Media Account:</h3>
+        <p>My name is Jhovella Mae G. Buendia, and I am 20 years old from Tunasan, Muntinlupa City. I am grateful to have this team, Group 4. I am a BSIT student who is still learning programming languages, particularly HTML and CSS.</p>
+        <img src="ella.jpg" alt="Jhovella Mae G. Buendia">
+        <button id="fb" onclick="window.location.href='https://www.facebook.com/jhovellamae.galiasbuendia?mibextid=ZbWKwL'"></button>
+        <button id="insta" onclick="window.location.href='https://www.instagram.com/itsyaboikielll?igsh=MWhqZzU4aWY1ejRpNg=='"></button>
+        <button id="github" onclick="window.location.href='https://github.com/jhovellamaee'"></button>
+    </div>
+    
+    <div id="content4" class="content">
+        <h2>Katrina M. Macalinao</h2>
+        <h3>My Social Media Account:</h3>
+        <p>Hi, my name is Katrina M. Macalinao from Team 4. I am a web developer from GMA Cavite with a passion for creating websites. With over 1 year of experience in HTML and CSS, I specialize in building responsive and accessible web applications.</p>
+        <img src="Kat.jpg" alt="Katrina M. Macalinao">
+        <button id="fb" onclick="window.location.href='https://www.facebook.com/katrina.macalinao.1?mibextid=ZbWKwL'"></button>
+        <button id="insta" onclick="window.location.href='https://www.instagram.com/_trnmcs?igsh=YmFyNzIzeDF6ZHAw'"></button>
+        <button id="github" onclick="window.location.href='https://github.com/Trinamacalinao'"></button>
+    </div>
+    
+    <div id="content5" class="content">
+        <h2>Jonalyn P. Madrigal</h2>
+        <h3>My Social Media Account:</h3>
+        <p>My name is Jonalyn P. Madrigal. I am 22 years old from Bayanan, Muntinlupa City. I am currently a 3rd-year college student at Pamantasan Lungsod ng Muntinlupa. I have a passion for learning basic CSS and HTML, and I am excited to be part of Team 4. I'm looking forward to learning and connecting with all of my team.</p>
+        <img src="Jo.jpg" alt="Jonalyn P. Madrigal">
+        <button id="fb" onclick="window.location.href='https://www.facebook.com/jonalyn.madrigal.52?mibextid=ZbWKwL'"></button>
+        <button id="insta" onclick="window.location.href='https://www.instagram.com/me_kyami?igsh=MW9wdWdhZXEyZGVpaA=='"></button>
+        <button id="github" onclick="window.location.href='https://github.com/Jonalynmadrigal'"></button>
+    </div>
+</body>
+</html>
+
